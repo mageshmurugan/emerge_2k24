89b27e2cb53774b25fdc053a34a36333e9ba5d33
Magesh <mageshmurugant@gmail.com>
2023-10-06 19:50:02 +0530

    11th

--- app/layout.js
+++ app/layout.js
@@ -5,14 +5,15 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Emerge-2k24',
-  description: 'National Level Technical Symposium',
+  description: 'National Level Technical Symposium'
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <meta  name="viewport" content="width=device-width, user-scalable=no"/>
+        <meta  name="viewport" content=
+            "width=device-width, user-scalable=no" />
       </head>
       <body className={inter.className}>{children}</body>
     </html>
