fab35a02360582088396f2240ee10503c1c4aa90
Magesh <mageshmurugant@gmail.com>
2023-10-07 09:45:35 +0530

    15th

--- app/components/Contents.jsx
+++ app/components/Contents.jsx
@@ -1,8 +1,8 @@
 import img1 from "../styles/img1.jpg";
-// import img2 from "../styles/img2.jpg";
-// import img3 from "../styles/img3.jpg";
-// import img4 from "../styles/img4.jpg";
-// import img5 from "../styles/img5.jpg";
+import img2 from "../styles/img2.jpg";
+import img3 from "../styles/img3.jpg";
+import img4 from "../styles/img4.jpg";
+import img5 from "../styles/img5.jpg";
 
 
 export const Content = [
@@ -10,15 +10,15 @@ export const Content = [
       heading: "CircuitTrix",
       paragraph:
         "CircuitTrix Symposium is an exciting event focused on technology and innovation. It brings together tech enthusiasts, engineers, and creative minds to explore the latest developments in electronics and circuit design.",
-      img: img1,
+      img: img4,
       tel:"tel:+919080151914",
       whatsapp:"https://chat.whatsapp.com/IJEyUEUd9m6EfdwwUnjpQX"
     },
     {
       heading: "Debugging",
       paragraph:
-        "CircuitTrix Symposium is an exciting event focused on technology and innovation. It brings together tech enthusiasts, engineers, and creative minds to explore the latest developments in electronics and circuit design.",
-      img: img1,
+        "Debugging competitions are engaging and educational events that challenge participants to hone their debugging skills while fostering a sense of camaraderie and competition within the programming community. These events are not only fun but also contribute to the development of highly skilled software professionals.",
+      img: img2,
       tel:"tel:+918270270603",
       whatsapp:"https://chat.whatsapp.com/BoS4r18ngiv3vPoaldXj8F"
 
@@ -27,7 +27,7 @@ export const Content = [
       heading: "Paper Presentation",
       paragraph:
         "Three rounds will be there .Winners of each round will be promoted to next round.First round will be general quiz where selection will be based on marks whereas the other two rounds selection will be based on the time the participants taketo complete the given task successfully.",
-      img: img1,
+      img: img3,
       tel:"tel:+916380358580",
       whatsapp:"https://chat.whatsapp.com/Ehyh0vsEibB00i5tlT0WfL"
 
@@ -37,7 +37,7 @@ export const Content = [
       heading: "Project Expo",
       paragraph:
         "Join us at Project Expo, where innovation meets inspiration! Discover cutting-edge projects, network with industry leaders, and witness the future unfold. Don't miss this opportunity to explore groundbreaking ideas and collaborate with the brightest minds in the field. Get ready to be inspired and amazed at Project Expo!",
-      img: img1,
+      img: img5,
       tel:"tel:+917339275059",
       whatsapp:"https://chat.whatsapp.com/J38xgU4CKLW97mzJnGBI6p"
 
